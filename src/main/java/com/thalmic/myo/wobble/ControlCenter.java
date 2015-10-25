a4dd974c0ed7a8af003b959b68f7b88c2a1a9810
akarle <ajk715@gmail.com>
2015-10-25 10:57:14 -0400

    uicolors

--- src/main/java/com/thalmic/myo/wobble/ControlCenter.java
+++ src/main/java/com/thalmic/myo/wobble/ControlCenter.java
@@ -28,41 +28,42 @@ public class ControlCenter extends JPanel {
         for(int i = 0; i < 8; i++) {
             if(messenger.isPlaying()){
                 if(i == messenger.getNotesIndex()) {
-                    g2.setColor(new Color(243, 31, 221,255));
-                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.setColor(new Color(86, 130, 3,255));
+                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     g2.setColor(Color.black);
-                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     //g2.drawString("" + notes[messenger.getNotesIndex()], centerOfPitchX, (windowHeight / 9 + 100 * (7 - messenger.getNotesIndex())));
                 }
                 else {
-                    g2.setColor(new Color(0, 243, 196, 240));
-                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.setColor(new Color(219, 215, 210));
+
+                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     g2.setColor(Color.black);
-                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     //g2.drawString("" + notes[i], centerOfPitchX, (windowHeight / 9 + 100 * (7 - i)));
                 }
             }
             else {
                 if(i == messenger.getNotesIndex()) {
-                    g2.setColor(new Color(172, 243, 101,240));
-                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.setColor(new Color(80, 114, 167,255));
+                    g2.fillRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     g2.setColor(Color.black);
-                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                     //g2.drawString("" + notes[messenger.getNotesIndex()], centerOfPitchX, (windowHeight / 8) * (7 - messenger.getNotesIndex()) + windowHeight/14);
                 }
                 else {
-                    g2.setColor(new Color(0, 243, 196,240));
-                    g2.fillRect(0, (7 - i) * windowHeight / 8, windowWidth / 2, windowHeight / 8);
+                    g2.setColor(new Color(219, 215, 210));
+                    g2.fillRect(0, (7 - i) * windowHeight / 8, windowWidth, windowHeight / 8);
                     g2.setColor(Color.black);
-                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth/2, windowHeight/8);
+                    g2.drawRect(0, (7-i)*windowHeight/8,windowWidth, windowHeight/8);
                 }
             }
 
             g2.setFont(new Font("default", Font.BOLD, 32));
             g2.drawString("" + notes[i], 20, (windowHeight / 8) * (7-i) + windowHeight/14);
             g2.setFont(new Font("default", Font.BOLD, 32));
-            g2.drawString("Pitch", windowWidth/5 + 30,windowHeight/20);
-            g2.drawString("Octave: "+messenger.getOctave(), 1200, 400);
+            g2.drawString("Pitch", windowWidth/2 - 25,windowHeight/20);
+            g2.drawString("Octave: "+messenger.getOctave(),  windowWidth/2 - 45, windowHeight-30);
         }
         //if(messenger.isPlaying()) {
         //    g2.setColor(Color.green);
